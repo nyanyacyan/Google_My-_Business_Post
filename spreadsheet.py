ac28195d248a9e30e31ceb8668cc9b2f92a33b2a
nyanyacyan <nyanyacyan@gmail.com>
2023-11-02 22:05:15 +0900

    スプレッドシート部分修正

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,7 +1,9 @@
 from logging import getLogger
 import pretty_errors
-from tqdm import tqdm
 from rich import print
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import week_calculator
 
 pretty_errors.activate
 
@@ -11,3 +13,48 @@ def log_message():
     logger.debug("debug message from spreadsheet.py")
     logger.info("info message from spreadsheet.py")
     logger.warning("warning message from spreadsheet.py")
+
+
+scopes = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+service_account_file = 'ここにJSONファイル名を入れる'
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name(service_account_file, scopes)
+
+gs = gspread.authorize(credentials)
+
+spreadsheet_key = '10RWjNIESE8VdK9DbWmix9cV5FmMeVuXuG31eZuQMuP4'
+spreadsheet = gs.open_by_key(spreadsheet_key)
+
+# それぞれのワークシートを定義
+worksheets_dict = {
+    "January": spreadsheet.worksheet("January"),
+    "February": spreadsheet.worksheet("February"),
+    "March": spreadsheet.worksheet("March"),
+    "April": spreadsheet.worksheet("April"),
+    "May": spreadsheet.worksheet("May"),
+    "June": spreadsheet.worksheet("June"),
+    "July": spreadsheet.worksheet("July"),
+    "August": spreadsheet.worksheet("August"),
+    "September": spreadsheet.worksheet("September"),
+    "October": spreadsheet.worksheet("October"),
+    "November": spreadsheet.worksheet("November"),
+    "December": spreadsheet.worksheet("December")
+}
+
+
+cell_addresses = {
+    1: 'B2',
+    2: 'B3',
+    3: 'B4',
+    4: 'B5',
+    5: 'B6'
+}
+
+
+current_month_name = week_calculator.current_month_name
+week_of_month = week_calculator.week_of_month
+
+# 辞書を使用して今月のワークシートを選択します
+current_worksheet = worksheets_dict[current_month_name]
+cell_value = cell_addresses[week_of_month]
+print(cell_value)
--- week_calculator.py
+++ week_calculator.py
@@ -1,6 +1,5 @@
 from logging import getLogger
 import pretty_errors
-from tqdm import tqdm
 from rich import print
 import datetime
 import calendar
@@ -15,19 +14,18 @@ def log_message():
     logger.warning("warning message from week_calculator.py")
 
 
+# 今日の日付から何週目かを出す
 def get_week_of_month(year, month, day):
-    '''
-    現在の月が何週あるかを出力する関数
-    calendarモジュールを使うと日曜日始まり。
-    カレンダーでweekリストを作成
-    index（０からスタート）に１をプラスすることで週数を割り出す。
-    '''
     month_calendar = calendar.monthcalendar(year, month)
     for index, week in enumerate(month_calendar):
         if day in week:
-            return index + 1
+            return index + 1  # indexは０スタートのため＋１
 
 today = datetime.date.today()
+current_month = today.month
+current_month_name = calendar.month_name[current_month]
+print(current_month_name)
 
 # 今日の日付がその月の何週目かを数値にて出力
 week_of_month = get_week_of_month(today.year, today.month, today.day)
+print(f'{week_of_month}週目')
