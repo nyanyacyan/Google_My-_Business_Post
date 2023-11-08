146424d495a02af7361cc32b970df8aa51015071
nyanyacyan <nyanyacyan@gmail.com>
2023-11-08 09:07:25 +0900

    修正

--- success_send.py
+++ success_send.py
@@ -4,16 +4,16 @@ from gmailsendsys import send_email
 def success_send():
     # 送信するメールの情報を設定
     # 送信元
-    from_email = 'nyanyacyan@gmail.com'
+    from_email = '*****@gmail.com'
 
     # メールを送る相手のアドレス
-    to_email = 'sakura.botan39@gmail.com'
+    to_email = '*****@gmail.com'
 
     # 件名
     subject = 'テストメール'
 
     # 本文
-    message = '今日も元気にやっていこう。'
+    message = '今日も最高でした。'
 
     # アプリパスワードを設定
     # やり方 Googleのアカウント→セキュリティ→2段階認証→アプリパスワード
